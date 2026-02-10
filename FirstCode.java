8d25b20245e273ee9e1298a53421940f17f0a20d
Jigoogle <ajaytripathi821@gmail.com>
2026-02-10 09:33:25 +0530

    First Commit is Nothing

---
+++ FirstCode.java
@@ -0,0 +1,19 @@
+class FirstCode {
+    public static void main(String args[]){
+        // System.out.print("Hello Ajay"); // in this it will adding each lines with same line but in using println() or \n backslash n is give us to next line 
+        // System.out.print("Hello World");
+        // System.out.println("Hello World \n From \n Apni Kakasha");// int his it will give us out put in next line 
+        // System.out.print("*\n**\n***\n****");
+        // System.out.println("1\n11\n111\n1111");
+
+// Variables
+         
+        int a = 45;
+        int b = 5;
+        int sum =  a+b;
+         
+        System.out.println(sum);
+
+    }
+
+}
