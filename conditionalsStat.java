8d60b03152d0de9adf47e8a27c9f0457d684d59f
Jigoogle <ajaytripathi821@gmail.com>
2026-02-12 16:08:23 +0530

    Today learned about If else Staatement

--- conditionalsStat.java
+++ conditionalsStat.java
@@ -1,20 +1,60 @@
-
 import java.util.Scanner;
-
-
 
 public class conditionalsStat {
     
 public static void main(String args[]) {
 
+    // Scanner sc = new Scanner(System.in);
+    // int age = sc.nextInt();
+
+    // if(age > 18){
+    //     System.out.println("Adult");
+
+    // }else{
+    //     System.out.println("Not Adult");
+    // }
     Scanner sc = new Scanner(System.in);
-    int age = sc.nextInt();
+    int button = sc.nextInt();
+    // int b = sc.nextInt();
 
-    if(age > 18){
-        System.out.println("Adult");
+    //If else 
 
-    }else{
-        System.out.println("Not Adult");
-    }
+    // if(a == b){
+    //     System.out.println("Equal");
+    // }else {
+    //     if(a>b){
+    //         System.out.println("a is Greater ");
+    //     }else{
+    //         System.out.println("a is lesser");
+    //     }      
+    // }
+
+    //Else If Else
+    // if(a == b ){
+    //     System.out.println("Equal ");
+    // }else if(a>b){
+    //     System.out.println("A is Greater ");
+    // }else{
+    //     System.out.println("A is Lesser");
+    // }
+// QS Print the Greeting 
+//using if else 
+//Hello
+// if(button == 1){
+//     System.out.println("Hello ");
+// }else if(button == 2){
+//     System.out.println("Nmaestey");
+// }else if(button == 3){
+//     System.out.println("Bonjure ");
+// }
+
+// Swithch statement 
+switch(button){
+    case 1:System.out.println("Hello ");
+    break;
+    case 2:System.out.println(" Namestey");
+    break;
+    case 3: System.out.println("Bonjure");
+}
 }
 }
