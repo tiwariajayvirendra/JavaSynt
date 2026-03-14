f45d4b7b8773d49fcb3490033d76e6208b5a1183
Jigoogle <ajaytripathi821@gmail.com>
2026-03-14 11:23:27 +0530

    Java For loop Learning and while Loop leanrnig only today sintex in this module but other due means practive is yet not done.

---
+++ Loops.java
@@ -0,0 +1,44 @@
+public class Loops {
+    public static void main(String args[]){
+//////////////////////////////////////////////////////////////////
+        // For LOOP 
+        // for(int counter = 0; counter < 100; counter = counter +1){
+        //     System.out.println("Hello Ajay");
+        // }
+//////////////////////////////////////////////////////////////////
+        // for (int counter = 0 ;counter <11; counter++){
+        //     System.out.println(counter+" ");
+        // }
+///////////////////////////////////////////////////////////////////
+            // for (int i=0; i<11;i++){
+            //     System.out.println(i);
+            // }
+/////////////////////////////////////////////////////////////////////////
+        // While LOOP 
+
+        // int i = 0;
+        // while (i < 11 ){
+        //     System.out.println(i);
+        //     i +=1 ; // i++
+        // }
+
+///////////////////////////////////////////////////////////////////
+//Do While Loop
+    // int i = 0;
+    // do { 
+    //     System.out.println(i);
+    //     i += 1;
+    // } while (i < 11);
+
+////////////////////////////////////
+    // int i = 12;
+    // while(i < 11){
+    //     System.out.println("Tiwari JI");
+    // }
+
+    // do {
+    //     System.out.println("Tiwari ajay ");
+        
+    // }while(i < 11);
+}
+}
--- conditionalsStat.java
+++ conditionalsStat.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class conditionalsStat {
     
 public static void main(String args[]) {
@@ -7,54 +5,56 @@ public static void main(String args[]) {
     // Scanner sc = new Scanner(System.in);
     // int age = sc.nextInt();
 
-    // if(age > 18){
-    //     System.out.println("Adult");
+//     // if(age > 18){
+//     //     System.out.println("Adult");
 
-    // }else{
-    //     System.out.println("Not Adult");
-    // }
-    Scanner sc = new Scanner(System.in);
-    int button = sc.nextInt();
-    // int b = sc.nextInt();
+//     // }else{
+//     //     System.out.println("Not Adult");
+//     // }
+//     Scanner sc = new Scanner(System.in);
+//     int button = sc.nextInt();
+//     // int b = sc.nextInt();
 
-    //If else 
+//     //If else 
 
-    // if(a == b){
-    //     System.out.println("Equal");
-    // }else {
-    //     if(a>b){
-    //         System.out.println("a is Greater ");
-    //     }else{
-    //         System.out.println("a is lesser");
-    //     }      
-    // }
+//     // if(a == b){
+//     //     System.out.println("Equal");
+//     // }else {
+//     //     if(a>b){
+//     //         System.out.println("a is Greater ");
+//     //     }else{
+//     //         System.out.println("a is lesser");
+//     //     }      
+//     // }
 
-    //Else If Else
-    // if(a == b ){
-    //     System.out.println("Equal ");
-    // }else if(a>b){
-    //     System.out.println("A is Greater ");
-    // }else{
-    //     System.out.println("A is Lesser");
-    // }
-// QS Print the Greeting 
-//using if else 
-//Hello
-// if(button == 1){
-//     System.out.println("Hello ");
-// }else if(button == 2){
-//     System.out.println("Nmaestey");
-// }else if(button == 3){
-//     System.out.println("Bonjure ");
+//     //Else If Else
+//     // if(a == b ){
+//     //     System.out.println("Equal ");
+//     // }else if(a>b){
+//     //     System.out.println("A is Greater ");
+//     // }else{
+//     //     System.out.println("A is Lesser");
+//     // }
+// // QS Print the Greeting 
+// //using if else 
+// //Hello
+// // if(button == 1){
+// //     System.out.println("Hello ");
+// // }else if(button == 2){
+// //     System.out.println("Nmaestey");
+// // }else if(button == 3){
+// //     System.out.println("Bonjure ");
+// // }
+
+//Switch statement 
+// switch(button){
+//     case 1:System.out.println("Hello ");
+//     break;
+//     case 2:System.out.println(" Namestey");
+//     break;
+//     case 3: System.out.println("Bonjure");
+// }
+// };
 // }
 
-// Swithch statement 
-switch(button){
-    case 1:System.out.println("Hello ");
-    break;
-    case 2:System.out.println(" Namestey");
-    break;
-    case 3: System.out.println("Bonjure");
-}
-}
-}
+
