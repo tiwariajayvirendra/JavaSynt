f1ffc7a3b27c460eb618addad8ac9d846c8a0ebe
Jigoogle <ajaytripathi821@gmail.com>
2026-03-15 16:36:57 +0530

    Patterns using java

--- Loops.java
+++ Loops.java
@@ -1,3 +1,5 @@
+
+
 public class Loops {
     public static void main(String args[]){
 //////////////////////////////////////////////////////////////////
@@ -88,8 +90,67 @@ public class Loops {
     //         }
             
     //     }
+    // //     System.out.println();
+    // }
+    // int n = 4;
+    // for (int i = 1 ; i<=n;i++){
+    //     //inner loop
+    //     for (int j= 1; j<=i ; j++){
+    //         System.out.print("*");
+    //     }
     //     System.out.println();
     // }
+    //print Gun StarF inverted Pyrimid 
+    // int n = 5;
+    // for (int i=n; i>=1;i--){
+    //     for (int j=1; j<=i;j++){
+    //       System.out.print("*");  
+    //     }
+    //   System.out.println();
+    // }
+// inverted half left pyrimid 
+    // int n = 5;
+    // for (int i=1; i<=n;i++){
+    //     //inner loop >>/Space Print
+    //     for (int j=1; j<=n-i;j++){   
+    //       System.out.print(" ");  
+    //     }
+    //     //inner loop >> star print
+    //     for (int j = 1 ; j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println( );
+            
+    // }
+
+
+    // int n = 5;
+    // for (int i = 1 ; i<=n;i++){
+    //     //inner loop
+    //     for (int j= 1; j<=i ; j++){
+             
+    //         System.out.print(j+" ");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    int n = 5;
+    // for (int i =n ; i>=1;i--){
+    //     //inner loop
+    //     for (int j= 1; j<=i ; j++){
+             
+    //         System.out.print(j+" ");
+    //     }
+    //     System.out.println();
+    // }
+
+    for (int i = 1 ; i<=n;i++ ){
+        for (int j= 1; j<=n-i+1;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
 
 }
 }
